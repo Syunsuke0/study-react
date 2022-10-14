33c2651f98575a042e715082092b579e3419ed17
Syunsuke Shima <s.syun617@icloud.com>
2022-10-14 12:20:43 +0900

    講座用のuseEffectの導入

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -3,7 +3,7 @@ import styles from 'src/styles/Home.module.css';
 import { Footer } from 'src/components/Footer';
 import { Main } from 'src/components/Main';
 import { Header } from 'src/components/Header';
-import { useCallback } from 'react';
+import { useCallback, useEffect } from 'react';
 
 // const handleClick = (e, foo) => {
 //   console.log(e.target.href);
@@ -20,15 +20,24 @@ export default function Home() {
     alert(foo);
   }, []);
 
+  useEffect(() => {
+    console.log('マウント時');
+    document.body.style.backgroundColor = 'lightblue';
+    return () => {
+      console.log('アンマウント時');
+      document.body.style.backgroundColor = '';
+    };
+  }, []);
+
   return (
     <div className={styles.container}>
       <Head>
         <title>Index Page</title>
       </Head>
       <Header />
-      {/* <a href="/about" onClick={handleClick}>
+      <a href="/about" onClick={handleClick}>
         ボタン
-      </a> */}
+      </a>
       <Main page="index" />
       <Footer />
     </div>
