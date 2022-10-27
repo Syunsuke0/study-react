0e78a4a63703bea72ed9613708d63a63675e1e43
Syunsuke Shima <s.syun617@icloud.com>
2022-10-27 23:39:01 +0900

    🎨 Layoutをコンポーネントを使ってまとめた

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,16 +1,15 @@
 import Head from 'next/head';
-import styles from 'src/styles/Home.module.css';
 import { Header } from 'src/components/Header';
 
 const Index = () => {
   return (
-    <div className={styles.container}>
+    <div>
       <Head>
         <title>Index page</title>
       </Head>
       <Header />
       <h1>Next.jsで学ぶReact講座</h1>
-      <p>JSONPlaceholderのAPIを色々叩いてみるよ！</p>
+      <p>JSONPlaceholderのAPIを色々叩いてみるよ!</p>
     </div>
   );
 };
