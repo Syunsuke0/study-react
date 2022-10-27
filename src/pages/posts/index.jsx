d645d5cdf140417ce3874800eabd8bb2f4a9d4f0
Syunsuke Shima <s.syun617@icloud.com>
2022-10-27 23:18:18 +0900

    🚚 投稿系ページの名前変更

--- src/pages/posts/index.jsx
+++ src/pages/posts/index.jsx
@@ -1,18 +1,18 @@
 import Head from 'next/head';
 import styles from 'src/styles/Home.module.css';
 import { Header } from 'src/components/Header';
-import { Posts } from 'src/components/Posts';
+import { Posts as PostsCompornent } from 'src/components/Posts';
 
-const Home = () => {
+const Posts = () => {
   return (
     <div className={styles.container}>
       <Head>
-        <title>Index Page</title>
+        <title>Posts Page</title>
       </Head>
       <Header />
-      <Posts />
+      <PostsCompornent />
     </div>
   );
 };
 
-export default Home;
+export default Posts;
